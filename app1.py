dde1d965b0e55fc338b5d23906375b9f009bab87
Lalitagauri Jitendra Walimbe <96299702+Lalitagauri19@users.noreply.github.com>
2022-11-27 10:42:31 +0530

    Add files via upload

---
+++ app1.py
@@ -0,0 +1,15 @@
+
+import streamlit as st
+
+st.title('Find whether the given number is odd or even.')
+placeholder_number = st.empty()
+
+n = placeholder_number.number_input('Enter The Number',step=1)
+
+if n>=0 and int(n)%2 != 0:
+    st.write("This is Odd number !! ")  
+elif n>=0 and int(n)%2 == 0:
+    st.write("This is Even number !! ")
+        
+else:
+    st.write(" Please Enter a valid Integer.")    
